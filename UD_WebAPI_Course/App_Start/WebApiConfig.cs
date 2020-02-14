59fd73bda5bc441e94f0da6c66583651922c781b
Lukasz Pieczonka <lukasz.pieczonka@globallogic.com>
2020-02-14 13:52:04 +0100

    Content Negotiations and Media Type Formatting

--- UD_WebAPI_Course/App_Start/WebApiConfig.cs
+++ UD_WebAPI_Course/App_Start/WebApiConfig.cs
@@ -5,6 +5,10 @@ using System.Web.Http;
 
 namespace UD_WebAPI_Course
 {
+    using System.Net.Http.Headers;
+
+    using Newtonsoft.Json;
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -17,8 +21,20 @@ namespace UD_WebAPI_Course
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+                defaults: new { id = RouteParameter.Optional });
+
+            // deleting XML formatter to give back JSON formatted return irrespectively of "Accept" HEADER value !
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            /* We want to implement :
+            If client requests from:
+            web browser - gets json
+            postman - he can specify the format xml/json
+            */
+
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            //sets indentation of a response
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
         }
     }
 }
