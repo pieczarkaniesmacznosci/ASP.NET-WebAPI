c47691ed9f3d868524901edc530b8f2ca0ef2afb
Lukasz Pieczonka <pieczarka51@gmail.com>
2020-02-27 22:24:12 +0100

    Reservations Controller created

---
+++ ExpressoAPI/Controllers/ReservationsController.cs
@@ -0,0 +1,24 @@
+﻿namespace ExpressoAPI.Controllers
+{
+    using System.Net;
+    using System.Web.Http;
+    using ExpressoAPI.Data;
+    using ExpressoAPI.Models;
+
+    public class ReservationsController : ApiController
+    {
+        EspressoDbContext espressoDbContext = new EspressoDbContext();
+
+        public IHttpActionResult Post(Reservation reservation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.BadRequest();
+            }
+
+            this.espressoDbContext.Reservations.Add(reservation);
+            this.espressoDbContext.SaveChangesAsync();
+            return StatusCode(HttpStatusCode.Created);
+        }
+    }
+}
